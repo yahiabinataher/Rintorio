62340196bc93c23180c37b290027808563eee103
yahiabinataher <yahiabinataher@gmail.com>
2024-08-21 19:27:55 +0300

    initilije done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Manrope': 'Manrope',
+      },
+      colors: {
+        'primary': '#ABEF5F',
+        'secend':'#212121',
+        'accent':'#737373'
+      },
+    },
+  },
+  plugins: [],
+}
